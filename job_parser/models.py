d76fca2a0593e03bb7cd0723b15c20e467f9a5c6
Jack Abernethy <jabber26@gmail.com>
2016-04-13 20:35:50 -0500

    Create models

--- job_parser/models.py
+++ job_parser/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+
+class JobPost(models.Model):
+    url = models.CharField(max_length=200)
+    position = models.CharField(max_length=150)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class Content(models.Model):
+    text = models.CharField(max_length=1000)
+    post_header = models.ForeignKey(PostHeader, on_delete=models.CASCADE)
+
+# will eventually add a foreign key for headers
+class PostHeader(models.Model):
+    orig_header = models.CharField(max_length=200)
+    post = models.ForeignKey(JobPost, on_delete=models.CASCADE)
