dc5bb0bbe70d5d8cc4bbb0826e940cbde3dbee60
Jack Abernethy <jabber26@gmail.com>
2016-04-13 20:47:33 -0500

    Add admin capacity for posts and companies

--- job_parser/models.py
+++ job_parser/models.py
@@ -11,11 +11,11 @@ class JobPost(models.Model):
     position = models.CharField(max_length=150)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
-class Content(models.Model):
-    text = models.CharField(max_length=1000)
-    post_header = models.ForeignKey(PostHeader, on_delete=models.CASCADE)
-
 # will eventually add a foreign key for headers
 class PostHeader(models.Model):
     orig_header = models.CharField(max_length=200)
     post = models.ForeignKey(JobPost, on_delete=models.CASCADE)
+
+class Content(models.Model):
+    text = models.CharField(max_length=1000)
+    post_header = models.ForeignKey(PostHeader, on_delete=models.CASCADE)
--- job_searcher/settings.py
+++ job_searcher/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'job_parser.apps.JobParserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
